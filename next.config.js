7e8c2d066d3f0c01c7a1c16f82f9d6e7acd138fd
Purwanto <purwanto@tlab.co.id>
2022-11-15 12:26:25 +0700

    change cd

--- next.config.js
+++ next.config.js
@@ -21,4 +21,7 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
   // Append the default value with md extensions
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  images: {
+    unoptimized: true,
+  },
 });
